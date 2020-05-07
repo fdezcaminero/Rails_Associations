c26239441bca256ebb803edf9f714a6ed12b820a
fdezcaminero <fdezcaminero@gmail.com>
2020-05-07 15:06:19 -0400

    Created Events Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_221132) do
+ActiveRecord::Schema.define(version: 2020_05_07_174849) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.date "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"

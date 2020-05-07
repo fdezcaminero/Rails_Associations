62ed5df41a42c85812a81100ad414bb17dcbe331
fdezcaminero <fdezcaminero@gmail.com>
2020-05-07 12:30:18 -0400

    Validations and Sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,20 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
     end
 
     def show
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end

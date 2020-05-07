cec38f313b45f670f25e2ab644e60063d9d3007d
elbie-em <emoonga1993@gmail.com>
2020-05-07 21:18:25 +0200

    Defined Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
+
+    has_many :events, foreign_key: :event_id, class_name: "Event"
 end

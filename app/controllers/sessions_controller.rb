0db46f19effb22d49b4f007f2aa16735055c778d
fdezcaminero <fdezcaminero@gmail.com>
2020-05-07 13:43:00 -0400

    Controllers and Views Configuration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:session][:username])
     if @user
-      session[:user_id] = user.id
-      redirect_to @user
+      session[:user_id] = @user.id
+      redirect_to new_user_path
+    else
+      flash.now[:error] = 'That username does not exist, please try again'
+      render :new
     end
   end
 
   def destroy
+    session.delete(:user_id)
+    current_user = nil
+    redirect_to new_user_path
   end
 end

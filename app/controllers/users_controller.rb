9c94c75cfa1845440079961422ccad12673b1d4b
elbie-em <emoonga1993@gmail.com>
2020-05-07 18:11:06 +0200

    Create sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
     def new
+        @user = User.new
     end
 
     def create
@@ -7,4 +9,5 @@ class UsersController < ApplicationController
 
     def show
     end
+
 end

c97bd9e1b9a2cbec09cf6fb3b205cd6a62987cbd
fdezcaminero <fdezcaminero@gmail.com>
2020-05-15 15:22:35 -0400

    Tests for Event Management Working!

--- spec/features/event_management_spec.rb
+++ spec/features/event_management_spec.rb
@@ -21,4 +21,64 @@ RSpec.describe "Events Management", type: :feature do
         visit events_path
         expect(page).to have_content('Example Party', 'My house')
     end
+
+    it 'is creating an event with invalid input (no name)' do
+        visit login_path
+        fill_in 'Username', with: 'example'
+        click_button 'Log In'
+        visit events_path
+        click_link 'Create Event'
+        visit new_event_path
+        fill_in 'Name', with: ''
+        fill_in 'Location', with: 'My house'
+        fill_in 'Day', with: Date.today + 90
+        fill_in 'Description', with: 'We will train you in the art of partying'
+        click_button 'Create Event'
+        expect(page).to have_content("Name can't be blank")
+    end
+
+    it 'is creating an event with invalid input (no location)' do
+        visit login_path
+        fill_in 'Username', with: 'example'
+        click_button 'Log In'
+        visit events_path
+        click_link 'Create Event'
+        visit new_event_path
+        fill_in 'Name', with: 'Example Party'
+        fill_in 'Location', with: ''
+        fill_in 'Day', with: Date.today + 90
+        fill_in 'Description', with: 'We will train you in the art of partying'
+        click_button 'Create Event'
+        expect(page).to have_content("Location can't be blank")
+    end
+
+    it 'is creating an event with invalid input (no day)' do
+        visit login_path
+        fill_in 'Username', with: 'example'
+        click_button 'Log In'
+        visit events_path
+        click_link 'Create Event'
+        visit new_event_path
+        fill_in 'Name', with: 'Example Party'
+        fill_in 'Location', with: 'My house'
+        fill_in 'Day', with: ''
+        fill_in 'Description', with: 'We will train you in the art of partying'
+        click_button 'Create Event'
+        expect(page).to have_content("Day can't be blank")
+    end
+
+    it 'is creating an event with invalid input (no description)' do
+        visit login_path
+        fill_in 'Username', with: 'example'
+        click_button 'Log In'
+        visit events_path
+        click_link 'Create Event'
+        visit new_event_path
+        fill_in 'Name', with: 'Example Party'
+        fill_in 'Location', with: 'My house'
+        fill_in 'Day', with: Date.today + 90
+        fill_in 'Description', with: ''
+        click_button 'Create Event'
+        expect(page).to have_content("Description can't be blank")
+    end
 end
